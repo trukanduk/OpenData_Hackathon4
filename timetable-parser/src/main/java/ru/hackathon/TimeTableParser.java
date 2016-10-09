3c1c240feb64919ea54c292845b0e058c2096f5d
test <test@test>
2016-10-09 12:33:32 +0300

    почистил расписание к Москве, объединил

--- timetable-parser/src/main/java/ru/hackathon/TimeTableParser.java
+++ timetable-parser/src/main/java/ru/hackathon/TimeTableParser.java
@@ -4,6 +4,7 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.google.common.base.Charsets;
 import com.google.common.base.Preconditions;
 import com.google.common.base.Splitter;
+import com.google.common.collect.Sets;
 import com.google.common.primitives.Ints;
 import org.apache.commons.csv.CSVFormat;
 import org.apache.commons.csv.CSVParser;
@@ -15,6 +16,7 @@ import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.HashSet;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
@@ -24,11 +26,11 @@ public class TimeTableParser {
 
 	public static final CSVFormat CSV_FORMAT = CSVFormat.newFormat('\t').withFirstRecordAsHeader();
 	public static final String STATION_NAME = "Остановочные пункты";
-	private final List<String> trains = new ArrayList<>();
 	private final Map<String, Timetable> timetables = new TreeMap<>();
 	private final ObjectMapper mapper = new ObjectMapper();
 
-	private void load(String filename) throws IOException {
+	private List<String> load(String filename, String direction) throws IOException {
+		final List<String> trains = new ArrayList<>();
 		try (CSVParser parser = CSVParser.parse(new File(filename), Charsets.UTF_8, CSV_FORMAT)) {
 			Map<String, Integer> headers = parser.getHeaderMap();
 			trains.addAll(headers.keySet());
@@ -52,29 +54,44 @@ public class TimeTableParser {
 					Iterator<String> timeStrParts = Splitter.on('.').split(timeStr).iterator();
 					int timeOfDeparture = Ints.tryParse(timeStrParts.next()) * 60 + Ints.tryParse(timeStrParts.next());
 					Timetable timetable = timetables.getOrDefault(train, new Timetable());
+					timetable.setDirection(direction);
 					timetable.getStations().add(new Pair(station, timeOfDeparture));
 					timetables.put(train, timetable);
 				}
 			}
 		}
+		return trains;
 	}
 
 	private void saveTimetable(String filename) throws IOException {
 		Files.write(Paths.get(filename),
-				Arrays.asList(mapper.writerWithDefaultPrettyPrinter().writeValueAsString(timetables)));
+				Arrays.asList(
+						"var timetable = " + mapper.writerWithDefaultPrettyPrinter().writeValueAsString(timetables)));
 	}
 
 	public static void main(String[] args) throws IOException {
 		TimeTableParser parser = new TimeTableParser();
-		parser.load(args[0]);
-		parser.saveTimetable(args[1]);
+		List<String> from = parser.load(args[0], "from_moscow");
+		List<String> to = parser.load(args[1], "to_moscow");
+		Preconditions.checkState(Sets.intersection(new HashSet<>(from), new HashSet<>(to)).isEmpty());
+		parser.saveTimetable(args[2]);
 	}
 
 	private static final class Timetable {
+		private String direction;
 		private List<Pair> stations = new ArrayList<>();
 
 		public List<Pair> getStations() {
 			return stations;
+		}
+
+		public void setDirection(String direction) {
+			this.direction = direction;
+		}
+
+		public String getDirection() {
+
+			return direction;
 		}
 	}
 
